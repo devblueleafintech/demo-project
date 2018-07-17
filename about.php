843a5dadba86d8551d7cd33124b9091b4c6b3193
Bhadresh Panchal <dev@blueleafintech.com>
2018-07-17 13:55:35 +0530

    changing second file

--- about.php
+++ about.php
@@ -79,6 +79,7 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>HOME | GTO Process</title>
 	<h2> Welcome to the GTO Batch 2018<h2/>
+		<p> The above 3 meta tags *must* come first in the head; any other head content must come *after* these tagsThe above 3 meta tags *must* come first in the head; any other head content must come *after* these tagsThe above 3 meta tags *must* come first in the head; any other head content must come *after* these tagsThe above 3 meta tags *must* come first in the head; any other head content must come *after* these tagsThe above 3 meta tags *must* come first in the head; any other head content must come *after* these tags<p/>
 
     <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
     <!-- Bootstrap -->
